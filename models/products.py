c66c10ff38b75fc60d0f5bfa8597a3d0001d377d
Richard Van Sickle <rvansickle91@gmail.com>
2017-02-10 09:36:08 -0600

    added comments to TestShoppingCart.py, products.py, and shoppingcart.py

--- models/products.py
+++ models/products.py
@@ -1,11 +1,23 @@
 class Product():
 
     def __init__(self, name, price):
+        """ 
+        Initializes the name and price for a product
+        author: Mark Ellis
+        """
         self.name = name
         self.price = price
 
     def get_product_name(self):
+        """
+        Returns the name of the product
+        author: Mark Ellis
+        """
         return self.product_name
 
     def get_product_price(self):
+        """
+        Returns price of particular product
+        author: Mark Ellis
+        """
         return self.product_price
