febbbe34f0ff6a09ec1da4877d3bd86bbc6b8e07
Mark Ellis <markellisdev@gmail.com>
2017-02-13 14:18:33 -0600

    correction in main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import sqlite3
 def init_db():
     """Initializes the sql file to create database"""
     with sqlite3.connect('bangazon.db') as conn:
-        # The following line creates the database
-        # f = open('populateDB.sql', 'r')
-        # The following line populates the database
-        f = open('fixture_populateDB.sql', 'r')
+        #-----> The following line creates the database-comment out after initial tables are created
+        f = open('populateDB.sql', 'r')
+        #-----> The following line populates the database - Comment in, after creating the table initially
+        # f = open('fixture_populateDB.sql', 'r')
         sql = f.read()
         c = conn.cursor()
         c.executescript(sql)
