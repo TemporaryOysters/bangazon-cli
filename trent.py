26e723fa3060566919fefa38da445711de620dd0
Trent Hand <livefree1983@gmail.com>
2017-02-14 13:27:47 -0600

    added leave_bangazon def to trent.py

--- trent.py
+++ trent.py
@@ -1,47 +1,26 @@
+import sys
+
 #Author: Trent Hand, Temporary Oysters
 #empty list to hold users and a blank status to hold the information
 users = {}
 status = ""
 
-#initial menu to 
-def displayMenu():
-    status = input("Are you a registered user? y/n? Press q to quit: ")  
-    if status == "y":
-        #calls the oldUser() function below
-        oldUser()
-    elif status == "n":
-        #prompts the user to create a new account by calling the function below
-        newUser()
-    return status
+
 
 def newUser():
-    createLogin = input("Create login name: ")
 
-    if createLogin in users: # check if login name exists
-        print("\nLogin name already exist!\n")
-        #these inputs will need to be passed into a Customer object from customer.py
-    else:
-        createPassw = input("Create password: ")
-        createName = input("Enter Customer name: ")
-        createAddress = input("Enter Street Address: ")
-        createCity = input("Enter City: ")
-        createState = input("Enter State: ")
-        createPostalCode = input("Enter Postal Code: ")
-        createPhoneNumber = input("Enter Phone Number: ")
-        users[createLogin] = createPassw # add login and password
+    print("press 'CTRL+C' to quit")
+    createName = input("Enter Customer name: ")
+    createAddress = input("Enter Street Address: ")
+    createCity = input("Enter City: ")
+    createState = input("Enter State: ")
+    createPostalCode = input("Enter Postal Code: ")
+    createPhoneNumber = input("Enter Phone Number: ")
 
-        print("\nUser created!\n")     
+    print("\nUser created!\n")  
+    #ADD THE METHOD TO BRING UP MAIN MENU   
+status = newUser()
 
-def oldUser():
-    login = input("Enter login name: ")
-    passw = input("Enter password: ")
-
-    # check if user exists and login matches password
-    if login in users and users[login] == passw: 
-        print("\nLogin successful!\n")
-    else:
-        print("\nUser doesn't exist or wrong password!\n")
-
-while status != "q":
-#keeps the user in the program until they type "q"            
-    status = displayMenu()
+#this code is designed to exit the program and should be called when the user selects option 7
+def leave_bangazon():
+    sys.exit("Thank you for visiting Bangazon!")
