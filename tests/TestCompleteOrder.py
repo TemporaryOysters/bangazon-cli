0382a54843af149231b0316979541ff70006c1bd
Joey Kirby <jkirbymusic@gmail.com>
2017-02-14 23:08:14 -0600

    fixed test bugs

--- tests/TestCompleteOrder.py
+++ tests/TestCompleteOrder.py
@@ -20,14 +20,15 @@ class TestCompleteOrder(unittest.TestCase):
         """
         Sets up a Test User, "Joey", so we can test if our code is behaving as expected.
         """
-        self.joey = customer.Customer('Joey', '787 East Silver St', 'Lebanon', 'Ohio', '35622', '5551231234', 'j@j.com')
-        self.joey.set_status_to_active()
-        self.joey_mastercard = payment_options.PaymentType("Mastercard", "1234", 5)
-        self.joey_mastercard.add_payment_type()
-        self.joeys_cart = shoppingcart.ShoppingCart("joey")
+        self.joe = customer.Customer('Joe llama', '787 Mulberry St', 'Nashville', 'TN', '35622', '5551231234', 'jllama@j.com')
+        self.joe.register_customer(self.joe)
+        self.joe.set_status_to_active()
+        self.joe_mastercard = payment_options.PaymentType("Mastercard", "1234", 5)
+        self.joe_mastercard.add_payment_type()
+        self.joes_cart = shoppingcart.ShoppingCart("joe")
         self.puppies = products.Product("puppies", 3)
-        self.joeys_cart.add_to_cart(self.puppies)
-        self.joeysorder = complete_order.Order()
+        self.joes_cart.add_to_cart(self.puppies)
+        self.joes_order = complete_order.Order()
 
 
     def test_order_is_complete(self):
@@ -36,21 +37,21 @@ class TestCompleteOrder(unittest.TestCase):
         An Order is 'complete' when it has attributes and values for a customer, a payment type, products in cart, and cart total cost.
         """
 
-        self.assertIsInstance(self.joeysorder, complete_order.Order)
+        self.assertIsInstance(self.joes_order, complete_order.Order)
 
     def test_order_has_proper_attributes(self):
         """
         Tests order is able to add correct CUSTOMER & PAYMENT TYPE attributes.
         """
-        self.assertIsNotNone(self.joeysorder.customer)
-        print("HEYYYY LOOK AT ME!", self.joeysorder.payment_type)
-        self.assertIsNotNone(self.joeysorder.payment_type)
+        self.assertIsNotNone(self.joes_order.customer)
+        print("HEYYYY LOOK AT ME!", self.joes_order.payment_type)
+        self.assertIsNotNone(self.joes_order.payment_type)
 
     def test_order_can_be_added_to_db(self):
         """
         Tests that orders can be stored in our DB.
         """
-        self.joeysorder.add_order_to_db(self.joeysorder)
-        self.assertTrue(self.joeysorder.is_order_in_db(self.joeysorder))
+        self.joes_order.add_order_to_db(self.joes_order)
+        self.assertTrue(self.joes_order.is_order_in_db(self.joes_order))
 
         
